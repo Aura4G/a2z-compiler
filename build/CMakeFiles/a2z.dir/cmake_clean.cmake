a973166e19e951ae39eb3621df3408204649c37c
Aura4G <aria4games@gmail.com>
2025-04-27 20:20:29 +0100

    very basic lexical analysis, tokenisation with executable generation

---
+++ build/CMakeFiles/a2z.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a2z.dir/src/main.cpp.o"
+  "CMakeFiles/a2z.dir/src/main.cpp.o.d"
+  "a2z"
+  "a2z.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a2z.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
